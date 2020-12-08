6d2fbadaab095f700c304588421b70d75f382157
Sai Playz <71771487+SaiAnshuman@users.noreply.github.com>
2020-12-08 17:48:56 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -4,8 +4,8 @@ player2_name = localStorage.getItem("Player2Name");
 player1_score = 0;
 player2_score = 0;
 
-document.getElementById("player1name").innerHTML = player1_name + ": ";
-document.getElementById("player2name").innerHTML = player2_name + ": ";
+document.getElementById("player1name").innerHTML = "Player1 = " + player1_name + ": ";
+document.getElementById("player2name").innerHTML = "Player2 = " + player2_name + ": ";
 document.getElementById("player1score").innerHTML = player1_score;
 document.getElementById("player2score").innerHTML = player2_score;
 
@@ -82,5 +82,7 @@ if(get_answer == actual_answer)
      document.getElementById("player_answer").innerHTML = "Answer Turn :- " + player1_name ;
 
   }
+
+  document.getElementById("output").innerHTML = "";
 
 }
